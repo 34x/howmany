e9cadec865a8719bb43a4989821254ff129aab7a
34x <maks@34x.me>
2015-07-04 20:37:27 +0200

    Initial Commit

---
+++ HowManyC/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  HowManyC
+//
+//  Created by Max on 04/07/15.
+//  Copyright (c) 2015 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ HowManyC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HowManyC
+//
+//  Created by Max on 04/07/15.
+//  Copyright (c) 2015 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
